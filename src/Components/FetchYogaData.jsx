3dc68558ec599963b302bfe35b4cf3042f35bc81
“Brent” <brentdwaller@gmail.com>
2024-12-03 13:39:26 -0500

    completed lab

--- src/Components/FetchYogaData.jsx
+++ src/Components/FetchYogaData.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import UseFetch from './UseFetch';
 import './FetchData.css';
 
-const FetchData = () => {
-    const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
+const FetchYogaData = () => {
+    const [data] = UseFetch('https://api.npoint.io/4459a9a10e43812e1152')
     console.log(data);
+    
     return (
         <>
         <ul className='list_data_main'>
-            <h1 className='usefetch_heading'>Food Data</h1>
+            <h1 className='usefetch_heading'>Yoga Poses</h1>
             {data && data.map((e) => (
                 <>
                     <li className='list_data'>
                         <h2>{e.name}</h2>
-                        <p><strong>Importance: </strong>{e.importance}</p>
                         <p><strong>Benefits: </strong>{e.benefits}</p>
-                        <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+                        <p><strong>Duration: </strong>{e.time_duration}</p>
                     </li>
                 </>
             ))}
@@ -24,4 +24,4 @@ const FetchData = () => {
     )
 }
 
-export default FetchData
+export default FetchYogaData;
